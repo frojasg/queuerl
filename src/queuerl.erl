72ddbc8b15d5bbd8116092237882937f3cbf4c5a
Milton Inostroza <milinostroza@groupon.com>
2016-03-18 12:54:22 -0500

    add todos

--- src/queuerl.erl
+++ src/queuerl.erl
@@ -2,9 +2,6 @@
 
 -export([new_task/1,
 	 enqueue/1,
-	 run/1,
-	 retry/2,
-	 notify_client/2,
 	 get_task/1]).
 
 new_task(Function) ->
@@ -12,15 +9,6 @@ new_task(Function) ->
 
 enqueue(Task) ->
   queuerl_task_actions:enqueue(Task).
-
-run(Task) ->
-  queuerl_task_actions:run(Task).
-
-retry(Task, RetryInfo) ->
-  queuerl_task_actions:retry(Task, RetryInfo).
-
-notify_client(Task, Info) ->
-  queuerl_task_actions:notify_client(Task, Info).
 
 get_task(TaskUuid) ->
   queuerl_controller:get_task(TaskUuid).
